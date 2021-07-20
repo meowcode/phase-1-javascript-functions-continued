d715677f1df3fcc4d012a83b7a57b0773844966c
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2021-07-20 19:38:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){//activity is an unknown variable in a math equation (ie a*b)
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun("bathe my dog")
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function(message="special"){
+        return `You are ${flair}${message}${flair}!`
+    }
+}
